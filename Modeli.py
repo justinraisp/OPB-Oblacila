80f74481de08b648bfefd83f79206ec82fbb5662
Blaž Arh <b.blazarh@gmail.com>
2023-05-04 13:38:29 +0200

    dodani vsi classi

--- Modeli.py
+++ Modeli.py
@@ -16,3 +16,39 @@ class Artikel:
     Status: str = field(default="")
     EAN: int = field(default=0)
     ManufacturerSKU: str = field(default="")
+
+@dataclass
+class StyleList:
+    Style: str = field(default="")
+    Name1: str = field(default="")
+    Name2: str = field(default="")
+    MaterialDescription: str = field(default="")
+    ProductDescription: str = field(default="")
+    CataloguePage: str = field(default="")
+    OrientationPrice: str = field(default="")
+    ItemCode: int = field(default=0)
+    Origin: str = field(default="")
+    Status: str = field(default="")
+    Categories: str = field(default="")
+
+
+@dataclass
+class Colour:
+    ColourCode: int = field(default=0)
+    ColourName: str = field(default="")
+    SortOrder: int = field(default=0)
+
+@dataclass
+class Size:
+    SizeCode: str = field(default="")
+    SizeName: str = field(default="")
+
+@dataclass
+class ColourTemplate:
+    ManufName: str = field(default="")
+    ManufCode: str = field(default="")
+    ColourName: str = field(default="")
+    CMYK: str = field(default="")
+    RGB: str = field(default="")
+
+
